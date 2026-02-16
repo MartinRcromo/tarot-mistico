0af028a6aeb57f660de614ea32dd56e65b538f54
MartinRcromo <martincromosol@gmail.com>
2026-02-16 15:07:20 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  distDir: 'dist',
   images: {
     unoptimized: true,
-  },
-  env: {
-    GEMINI_API_KEY: process.env.GEMINI_API_KEY,
   },
 }
 
